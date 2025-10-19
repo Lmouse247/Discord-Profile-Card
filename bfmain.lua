f429c685c4216f075f2690592e3093e7920a85c6
Lmouse247 <137436456+Lmouse247@users.noreply.github.com>
2025-10-19 11:37:48 +0700

    Update bfmain.lua

--- bfmain.lua
+++ bfmain.lua
@@ -4,24 +4,3 @@ getgenv().NewUI = false
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/UGPhone's%20Scripts"))()
 
 
-getgenv().Hermanos_Settings = {
-
-    ['key'] = '6288d035-322e-4a94-8803-bbb2344028cf',
-    ['PC'] = 'CHANGE-ME',
-
-
-    ['webhooks'] = {
-        ['fullmoon'] = 'https://discord.com/api/webhooks/',
-        ['mirage'] = 'https://discord.com/api/webhooks/',
-    },
-    ['Sword'] = {'Cursed Dual Katana', 'Shark Anchor', 'Tushita', 'Yama', 'Dark Dagger', 'Hallow Scythe', 'Saber'},
-    ['Gun'] = {'Soul Guitar', 'Serpent Bow', 'Kabucha', 'Acidum Rifle'},
-    ['Accessories'] = {'Dark Coat', 'Leviathan Shield', 'Leviathan Crown', 'Pale Scarf', 'Kitsune Mask', 'Kitsune Ribbon'},
-    ['Fruit'] = {
-        'Kitsune', 'Leopard', 'Dragon (West)', 'Spirit', 'Control', 'Venom', 'Gas', 'Yeti',
-        'Shadow', 'Dough', 'Mammoth', 'T-Rex', 'Dragon (East)'
-    }
-
-}
- 
-task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/main.lua'))() end)
